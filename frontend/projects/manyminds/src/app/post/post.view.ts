2803e91ae76a0211a91d617e32aad33c9fa1123e
Eric Eslinger <eric.eslinger@gmail.com>
2023-12-29 11:21:18 -0800

    posts

---
+++ frontend/projects/manyminds/src/app/post/post.view.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mm-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './post.view.html',
+  styleUrl: './post.view.scss'
+})
+export class PostView {
+
+}
